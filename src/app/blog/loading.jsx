7ce5e481e274e90df9b7372f76ff943557082c1c
yahayagodiwa <yahayagodiwa8@gmail.com>
2025-05-16 11:33:06 +0100

    loading added

---
+++ src/app/blog/loading.jsx
@@ -0,0 +1,12 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Loader;
+
